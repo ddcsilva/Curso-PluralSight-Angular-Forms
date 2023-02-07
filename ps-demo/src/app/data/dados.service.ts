f2d6feb9c0466cc92ad7bea06f3a7a19468e59c6
Danilo Silva <danilo.silva@msn.com>
2023-02-07 05:51:49 -0300

    Criando Service para tratar dados

---
+++ ps-demo/src/app/data/dados.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { ConfiguracoesUsuario } from './configuracoes-usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DadosService {
+
+  constructor() { }
+
+  postFormularioConfiguracoesUsuario(configuracoesUsuario: ConfiguracoesUsuario) {
+    
+  }
+}
