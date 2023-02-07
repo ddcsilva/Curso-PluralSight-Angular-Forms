229fb80da487213ee1e03409f1027807067d6bcc
Danilo Silva <danilo.silva@msn.com>
2023-02-07 06:11:14 -0300

    Post no Formulário

--- ps-demo/src/app/data/dados.service.ts
+++ ps-demo/src/app/data/dados.service.ts
@@ -11,7 +11,7 @@ export class DadosService {
   constructor(private http: HttpClient) { }
 
   postFormularioConfiguracoesUsuario(configuracoesUsuario: ConfiguracoesUsuario) : Observable<any> {
-    return this.http.post("url", configuracoesUsuario)
+    return this.http.post("https://putsreq.com/Tzanc8NLL4I9vdD9UXhH", configuracoesUsuario)
     
     // return of(configuracoesUsuario);
   }
